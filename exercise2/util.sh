a1240c045a55eccdaacb20a3e42ee3ff8350b57d
Vinicius Renan de Carvalho <viniciusrenan.carvalho@gmail.com>
2022-11-30 17:53:29 -0300

    xargs

--- exercise2/util.sh
+++ exercise2/util.sh
@@ -5,7 +5,7 @@ create_stack(){
     file_name=$2
     extra_parameters=$3
     echo "Create Bucket Stack "$stack_name" using file: "$file_name
-    aws cloudformation create-stack --stack-name $stack_name  --template-body file://$file_name $extra_parameters --capabilities CAPABILITY_IAM
+    aws cloudformation create-stack --stack-name $stack_name  --template-body file://$file_name $extra_parameters --capabilities CAPABILITY_IAM | xargs
     echo "Wait for the creation"
     aws cloudformation wait stack-create-complete --stack-name $stack_name
 }
